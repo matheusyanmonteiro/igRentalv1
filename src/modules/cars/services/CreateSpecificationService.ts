286a70f5c783836a1aa5e523c45a98a31f36f559
Matheus <matheusyanmonteiro@gmail.com>
2021-03-28 18:15:03 -0300

    created specifications routes

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificatiosRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+export { CreateSpecificationService };
