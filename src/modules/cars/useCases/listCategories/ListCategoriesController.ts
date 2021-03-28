e724806d85dbbc13c17714e63416243caf570922
Matheus <matheusyanmonteiro@gmail.com>
2021-03-28 19:01:32 -0300

    separeted routes in useCases folders

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,15 @@
+import { Response, Request } from "express";
+
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  constructor(private listCategoriesUsecase: ListCategoriesUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const all = this.listCategoriesUsecase.execute();
+
+    return response.json(all);
+  }
+}
+
+export { ListCategoriesController };
