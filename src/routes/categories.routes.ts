690e5a42bc1b0cfcfca10772a71b4e99cc170be4
v <matheusyanmonteiro@gmail.com>
2021-04-23 23:14:50 -0300

    refactor all code and fix many problems with database

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import createCategoryController from "../modules/cars/useCases/createCategory";
 import { importCategoryController } from "../modules/cars/useCases/importCategory/Index";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
@@ -10,17 +10,17 @@ const categoriesRoutes = Router();
 const upload = multer({
   dest: "./tmp",
 });
-
+console.log("here i go");
 categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
-categoriesRoutes.get("/", (request, response) => {
-  return listCategoriesController.handle(request, response);
-});
+// categoriesRoutes.get("/", (request, response) => {
+//   return listCategoriesController.handle(request, response);
+// });
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController.handle(request, response);
-});
+// categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+//   return importCategoryController.handle(request, response);
+// });
 
 export { categoriesRoutes };
